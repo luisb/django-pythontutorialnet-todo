b250d46cd6f0a1cea52e4d9ceaa3a7ffb79cce37
Luis Baquera <luisb@ucr.edu>
2024-07-11 19:28:05 -0700

    Create Task UpdateView and update templates

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib import messages
 from django.urls import reverse_lazy
 
@@ -28,4 +28,12 @@ class TaskCreate(CreateView):
         form.instance.user = self.request.user
         messages.success(self.request, "The task was created successfully.")
         return super(TaskCreate, self).form_valid(form)
-    
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TaskUpdate, self).form_valid(form)
