2afd52fc219ab1061fb3feaec9c92a98b930707d
Luis Baquera <luisb@ucr.edu>
2024-07-11 18:05:00 -0700

    Create Task ListView; create/update templates

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import home, TaskList
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
+    path('tasks/', TaskList.as_view(), name='tasks'),
 ]
