d6a8b7a7f0bbd37c2f2262540e9ad490cac9298c
Luis Baquera <luisb@ucr.edu>
2024-07-11 19:46:28 -0700

    Create Task DeleteView and create/update templates

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import home, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('task/create/', TaskCreate.as_view(),name='task-create'),
     path('task/update/<int:pk>/', TaskUpdate.as_view(),name='task-update'),
+    path('task/delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
